c68851c207f6ad9573a0942adacd32e5ce8b979b
ravitejam66 <raviteja.m@SSLTUIODC.LOCAL>
2018-10-29 18:39:09 +0530

    Product detail page data parts integrated

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import {RestapisService} from '../services/restapis.service';
-import * as Immutable from 'immutable';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
@@ -10,38 +9,20 @@ import * as Immutable from 'immutable';
 export class ProductComponent implements OnInit {
   id: number;
   private sub: any;
+  price:any;
+  productName: any;
+  productCode:any;
+  specs: any;
+  sectorName: any;
+  industryName: any;
+  availability: any;
+  productSpecs: any;
   constructor(private route: ActivatedRoute,
              private _apiService: RestapisService) { }
-             options = Immutable.Map({
-              showDots: true,         // Shows a dot navigation component
-              height: 450,            // The initial slideshow height
-              showThumbnails: true,   // Optionally include thumbnails a navigation option
-              thumbnailWidth: 150     // Thumbnail individual width for the thumbnail navigation component
-            });
 
-            images = Immutable.List([
-              {
-                url: 'http://placekitten.com/800/500',
-                title: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. ' +
-                'Lorem Ipsum has been the industrs standard dummy text ever since the 1500s, when an unknown printer ' +
-                'took a galley of type and scrambled it to make a type specimen book.'
-              },
-              {url: 'http://placekitten.com/1200/500'},
-              {url: 'http://placekitten.com/800/300', title: 'cat'},
-              {url: 'http://placekitten.com/300/500', title: 'cat'},
-              {url: 'http://placekitten.com/860/500', title: 'cat'},
-              {url: 'http://placekitten.com/830/500', title: 'cat'},
-              {url: 'http://placekitten.com/660/500', title: 'cat'},
-              {url: 'http://placekitten.com/720/500', title: 'cat'},
-              {url: 'http://placekitten.com/360/300', title: 'cat'},
-              {url: 'http://placekitten.com/860/860', title: 'cat'},
-              {url: 'http://placekitten.com/800/900', title: 'cat'},
-              {url: 'http://placekitten.com/800/1200', title: 'cat'}
-            ]);
   ngOnInit() {
     this.sub = this.route.snapshot.queryParams['p'];
     this.getProductDetail();
-      console.log(atob(this.sub));
   }
 
   public getProductDetail() {
@@ -52,6 +33,25 @@ export class ProductComponent implements OnInit {
       };
 
     this._apiService.postApiResponse('/i4gorigin.listing/getPageProductDetail', productDetial).subscribe( data => {
+      if(data){
+       this.productName = data.Product.ProductName;
+       this.sectorName = data.Product.SectorName;
+       this.industryName = data.Product.IndustryName;
+       this.productSpecs = data.Product.Specs.Spec;
+       if(data.Product.SampleAvailability == 'Y'){
+        this.availability = true;
+       } else {
+        this.availability = false;
+       }
+       
+       this.productCode = data.Product.I4gProductCode;
+       if(data.Product.Price == "" || data.Product.Price == undefined){
+        this.price = 0.00;
+       } else {
+        this.price = data.Product.Price;
+       }
+       
+      }
       console.log(data);
     }, error => {
     });
